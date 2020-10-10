849b4f9173991b1d0a9dc615078b4262b2b16ba2
stefan4472 <stefan.kussmaul@gmail.com>
2020-10-10 10:31:04 -0400

    Simple starting GameRenderer class to draw at world offsets

---
+++ src/game_renderer.cpp
@@ -0,0 +1,75 @@
+#include "game_renderer.h"
+
+GameRenderer::GameRenderer(
+        SDL_Renderer* renderer,
+        std::shared_ptr<TextureCache> textureCache,
+        int gameWidth,
+        int gameHeight
+) {
+    this->textureCache = textureCache;
+    this->gameWidth = gameWidth;
+    this->gameHeight = gameHeight;
+    topLeftWorldX = 0.0;
+    topLeftWorldY = 0.0;
+}
+
+void GameRenderer::setTopLeftCoord(
+        double worldX, 
+        double worldY
+) {
+    topLeftWorldX = worldX;
+    topLeftWorldY = worldY;
+}
+
+void GameRenderer::drawToWorld(
+        TextureId textureId, 
+        double worldX, 
+        double worldY
+) {
+    // TODO: INEFFICIENT TO QUERY DIMENSIONS EACH DRAW()
+    int texture_width, texture_height;
+    std::tie(texture_width, texture_height) = 
+        textureCache->getDimensions(textureId);
+
+    SDL_Rect src = {
+        0,
+        0,
+        texture_width,
+        texture_height
+    };
+
+    SDL_Rect dest = {
+        static_cast<int>(worldX - topLeftWorldX),
+        static_cast<int>(worldY - topLeftWorldY),
+        texture_width,
+        texture_height
+    };
+
+    SDL_RenderCopy(
+        renderer,
+        textureCache->getTexture(textureId),
+        &src,
+        &dest
+    );
+}
+
+void GameRenderer::drawToWorld(
+        TextureId textureId,
+        SDL_Rect* subRect,
+        double worldX,
+        double worldY
+) {
+    SDL_Rect dest = {
+        static_cast<int>(worldX - topLeftWorldX),
+        static_cast<int>(worldY - topLeftWorldY),
+        subRect->w,
+        subRect->h
+    };
+
+    SDL_RenderCopy(
+        renderer,
+        textureCache->getTexture(textureId),
+        subRect,
+        &dest
+    );
+}
---
+++ src/game_renderer.h
@@ -0,0 +1,43 @@
+#ifndef _GAME_RENDERER_H
+#define _GAME_RENDERER_H
+
+#include <SDL2/SDL.h>
+#include "texture_cache.h"
+
+
+class GameRenderer
+{
+public:
+    GameRenderer(
+        SDL_Renderer* renderer,
+        std::shared_ptr<TextureCache> textureCache,
+        int gameWidth,
+        int gameHeight
+    );
+    
+    void setTopLeftCoord(
+        double worldX, 
+        double worldY
+    );
+
+    void drawToWorld(
+        TextureId textureId, 
+        double worldX, 
+        double worldY
+    );
+
+    void drawToWorld(
+        TextureId textureId,
+        SDL_Rect* subRect,
+        double worldX,
+        double worldY
+    );
+
+private:
+    SDL_Renderer* renderer;
+    std::shared_ptr<TextureCache> textureCache;
+    int gameWidth, gameHeight;
+    double topLeftWorldX, topLeftWorldY;
+};
+
+#endif
