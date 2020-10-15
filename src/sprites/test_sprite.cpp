3ec5b178db165cf02bbf60ae95acb8e72f0366d6
stefan4472 <stefan.kussmaul@gmail.com>
2020-10-15 11:08:12 -0400

    Initial Sprite class implementation for upcoming changes
    (for the Actor/Sprite split)

---
+++ src/sprites/test_sprite.cpp
@@ -0,0 +1,166 @@
+#include "test_sprite.h"
+
+TestSprite::TestSprite(
+        SpriteType spriteType, 
+        std::shared_ptr<SpriteModel> spriteModel,
+        // int hp,
+        double worldX, 
+        double worldY
+) {
+    this->spriteType = spriteType;
+    this->spriteModel = spriteModel;
+    this->worldX = worldX;
+    this->worldY = worldY;
+
+    walkPxPerMs = TextureCache::TILE_SIZE_PX * 1.0 / TILE_WALK_TIME_MS;
+    currWalkCommand = Direction::NONE;
+    goalWorldX = worldX;
+    goalWorldY = worldY;
+}
+
+SpriteType TestSprite::getSpriteType()
+{
+    return spriteType;
+}
+
+void TestSprite::setInHandItem(std::shared_ptr<Item> item)
+{
+    inHandItem = item;
+}
+
+std::pair<double, double> TestSprite::getWorldCoords()
+{
+    return std::make_pair(
+        worldX,
+        worldY
+    );
+}
+
+bool TestSprite::getIsWalking()
+{
+    return (currWalkCommand != Direction::NONE);
+}
+
+void TestSprite::walkUp(int numPx)
+{
+    currWalkCommand = Direction::UP;
+    goalWorldX = worldX;
+    goalWorldY = worldY - numPx;
+    spriteModel->moveUp();
+}
+
+void TestSprite::walkDown(int numPx)
+{
+    currWalkCommand = Direction::DOWN;
+    goalWorldX = worldX;
+    goalWorldY = worldY + numPx;
+    spriteModel->moveDown();
+}
+
+void TestSprite::walkLeft(int numPx)
+{
+    currWalkCommand = Direction::LEFT;
+    goalWorldX = worldX - numPx;
+    goalWorldY = worldY;
+    spriteModel->moveLeft();
+}
+
+void TestSprite::walkRight(int numPx)
+{
+    currWalkCommand = Direction::RIGHT;
+    goalWorldX = worldX + numPx;
+    goalWorldY = worldY;
+    spriteModel->moveRight();
+}
+
+void TestSprite::update(int msSincePrevUpdate)
+{
+    // Move sprite, if currently walking
+    switch (currWalkCommand)
+    {
+        case Direction::UP:
+        {
+            worldY -= walkPxPerMs * msSincePrevUpdate;
+            // Check for completion (and fix possible overshoot)
+            if (worldY <= goalWorldY)
+            {
+                worldY = goalWorldY;
+                currWalkCommand = Direction::NONE;
+                spriteModel->stopMoving();
+            }
+            break;
+        }
+        case Direction::DOWN:
+        {
+            worldY += walkPxPerMs * msSincePrevUpdate;
+            // Check for completion (and fix possible overshoot)
+            if (worldY >= goalWorldY)
+            {
+                worldY = goalWorldY;
+                currWalkCommand = Direction::NONE;
+                spriteModel->stopMoving();
+            }
+            break;
+        }
+        case Direction::LEFT:
+        {
+            worldX -= walkPxPerMs * msSincePrevUpdate;
+            // Check for completion (and fix possible overshoot)
+            if (worldX <= goalWorldX)
+            {
+                worldX = goalWorldX;
+                currWalkCommand = Direction::NONE;
+                spriteModel->stopMoving();
+            }
+            break;
+        }
+        case Direction::RIGHT:
+        {
+            worldX += walkPxPerMs * msSincePrevUpdate;
+            // Check for completion (and fix possible overshoot)
+            if (worldX >= goalWorldX)
+            {
+                worldX = goalWorldX;
+                currWalkCommand = Direction::NONE;
+                spriteModel->stopMoving();
+            }
+            break;
+        }
+        case Direction::NONE:
+        {
+            break;
+        }
+    }
+
+    // Update model
+    spriteModel->update(msSincePrevUpdate);
+}
+
+void TestSprite::draw(GameRenderer* renderer)
+{
+    // Get drawing information from spriteModel
+    TextureId texture_id;
+    SDL_Rect src_rect;
+    std::tie(texture_id, src_rect) = spriteModel->getDrawInfo();
+
+    // Draw model
+    renderer->drawToWorld(
+        texture_id,
+        src_rect,
+        worldX - src_rect.w / 2,
+        worldY - src_rect.h
+    );
+
+    // Draw in-hand item
+    // (currently drawing at an offset so as to give a rough 
+    // approximation that the item is actually in the Sprite's 
+    // hand.
+    if (inHandItem)
+    {
+        renderer->drawToWorld(
+            inHandItem->getTextureId(),
+            worldX + src_rect.w / 2 - 10,
+            worldY - src_rect.h / 2 - 10
+        );
+    }
+}
---
+++ src/sprites/test_sprite.h
@@ -0,0 +1,80 @@
+#ifndef _TEST_SPRITE_H
+#define _TEST_SPRITE_H
+
+#include <SDL2/SDL.h>
+#include <memory>
+#include "game_context.h"
+#include "update_context.h"
+#include "game_renderer.h"
+#include "sprite_type.h"
+#include "sprite_model.h"
+#include "item.h"
+
+class TestSprite
+{
+public:
+    TestSprite(
+        SpriteType spriteType, 
+        std::shared_ptr<SpriteModel> spriteModel,
+        // int hp,
+        double worldX, 
+        double worldY
+    );
+
+    // How long it takes to walk the distance of one tile
+    const int TILE_WALK_TIME_MS = 400;
+
+    SpriteType getSpriteType();
+    
+    void setInHandItem(std::shared_ptr<Item> item);
+
+    std::pair<double, double> getWorldCoords();
+
+    // TODO    
+    // int getHp();
+
+    // NOTE: ALWAYS MAKE SURE `ISWALKING()` IS FALSE BEFORE SENDING
+    // THE NEXT WALK COMMAND! OTHERWISE, THE SPRITE WILL NO LONGER
+    // BE TILE-ALIGNED
+    bool getIsWalking();
+
+    // void walkOneTileUp();
+    // void walkOneTileDown();
+    // void walkOneTileLeft();
+    // void walkOneTileRight();
+
+    // OR?
+    void walkUp(int numPx);
+    void walkDown(int numPx);
+    void walkLeft(int numPx);
+    void walkRight(int numPx);
+
+    // Update
+    void update(int msSincePrevUpdate);
+    
+    // Draw self onto provided renderer
+    // TODO: THIS WILL BE CHANGED IN FAVOR OF RETURNING A LIST
+    // OF DRAW COMMANDS, OR TO AT LEAST PROVIDE A BOUNDING BOX.
+    // THAT WAY, THE ACTOR CAN DRAW A HEALTHBAR, PARTICLES, ETC.
+    // ON THE SPRITE
+    void draw(GameRenderer* renderer);
+
+protected:
+    SpriteType spriteType;
+    
+    // World coordinates
+    double worldX, worldY;
+
+    std::shared_ptr<Item> inHandItem;
+
+    std::shared_ptr<SpriteModel> spriteModel;
+
+    // Number of pixels walked per millisecond
+    double walkPxPerMs;
+    
+    // Direction currently being walked in
+    Direction currWalkCommand;
+    double goalWorldX, goalWorldY;
+};
+
+#endif
